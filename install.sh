0dd2874db2b0cebca49db9962ef46220f927d59d
Roach Reznov <xtanveerx@gmail.com>
2016-07-28 20:57:38 +0530

    Added Backup

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 case $1 in
 	--uninstall) sudo rm /usr/sbin/cread /usr/sbin/creds /usr/sbin/hostap /usr/sbin/macspoof /usr/sbin/wich /usr/sbin/wich_bak /usr/sbin/wifijam; exit 1;;
-	*) sudo apt-get update; sudo apt-get install libc6 dpkg hostapd hostap-utils net-tools perl python2.7 bash; sudo mv ./usr/sbin/cread /usr/sbin/; sudo mv ./usr/sbin/creds /usr/sbin/; sudo mv ./usr/sbin/hostap /usr/sbin/; sudo mv ./usr/sbin/macspoof /usr/sbin/; sudo mv ./usr/sbin/wich /usr/sbin/; sudo mv ./usr/sbin/wich_bak /usr/sbin/; sudo mv ./usr/sbin/wifijam /usr/sbin/; sudo chmod +x /usr/sbin/cread /usr/sbin/creds /usr/sbin/macspoof /usr/sbin/wich /usr/sbin/wich_bak /usr/sbin/wifijam; exit 1;;
+	*) sudo apt-get update; sudo apt-get install libc6 dpkg hostapd hostap-utils net-tools perl python2.7 bash; sudo cp ./usr/sbin/cread /usr/sbin/; sudo cp ./usr/sbin/creds /usr/sbin/; sudo cp ./usr/sbin/hostap /usr/sbin/; sudo cp ./usr/sbin/macspoof /usr/sbin/; sudo cp ./usr/sbin/wich /usr/sbin/; sudo cp ./usr/sbin/wich_bak /usr/sbin/; sudo cp ./usr/sbin/wifijam /usr/sbin/; sudo chmod +x /usr/sbin/cread /usr/sbin/creds /usr/sbin/macspoof /usr/sbin/wich /usr/sbin/wich_bak /usr/sbin/wifijam; exit 1;;
 esac
